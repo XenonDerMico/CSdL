6e5d259829e55d3b78177b9a645e3eb46956d3d8
Salabasti <salabasti@Bastis-neues-MacBook-Pro.local>
2017-07-04 17:41:32 +0200

    added generic functions and conway constructor

--- FrederikConway.r
+++ FrederikConway.r
@@ -1,3 +1,28 @@
+conway <- function(type = "rPentomino", rule = "23/3", ...) {
+  x <- switch(type,
+         "random" = {
+           Zufallsmatrix(...)
+         },
+         "gleiter" = {
+           gleiter()
+         },
+         "gleiter4" = {
+           gleiter4()
+         },
+         "explusion" = {
+           explusion()
+         },
+         "rPentomino" = {
+           rPentomino()
+         },
+         "custom" = {
+           matrix(...)
+         })
+  C <- list(field = x, rules = rule)
+  class(C) <- c("conway")
+  return(C)
+}
+
 #' create a random start matrix for CGoL
 #' 
 #' @param x number of rows
@@ -7,7 +32,7 @@
 #' @return a matrix with \code{x} rows and \code{y} columns, with random values {0, 1} of type \code{type} based on the probability \code{p}
 #' @examples
 #' Zufallsmatrix(10, 10, 0.5)
-Zufallsmatrix <- function(x,y,p, type = "double"){
+Zufallsmatrix <- function(x = 10, y = 10, p = 0.5, type = "double"){
   
   # Zufallsmatrix erzeugt eine x*y große Matrix deren Einträge mit einer
   # Warscheinlichkeit p 1./1/T und sonst 0./0/F sind
@@ -20,6 +45,8 @@ Zufallsmatrix <- function(x,y,p, type = "double"){
   }else{
     M <- matrix((runif(x*y)<=p),x,y) 
   }
+  
+  class(M) <- "conway"
   
   return(M)
 }
@@ -47,24 +74,23 @@ alsRegelliste <- function(regelstring){
 #' @param 
 #' @return 
 #' @examples
-Nachbarn <- function(M){
+Nachbarn <- function(con){
   
   # Gibt eine Matrix der Größe von M+2 zurück welche die Anzahl an Nachbar enthält
   
-  m <- array(0, dim(M)+2)
+  m <- array(0, dim(con$field)+2)
   
-  m[1:nrow(M),1:ncol(M)] <-m[1:nrow(M),1:ncol(M)]+M
-  m[1:nrow(M),1:ncol(M)+1] <-m[1:nrow(M),1:ncol(M)+1]+M
-  m[1:nrow(M),1:ncol(M)+2] <-m[1:nrow(M),1:ncol(M)+2]+M
+  m[1:nrow(con$field),1:ncol(con$field)] <-m[1:nrow(con$field),1:ncol(con$field)]+con$field
+  m[1:nrow(con$field),1:ncol(con$field)+1] <-m[1:nrow(con$field),1:ncol(con$field)+1]+con$field
+  m[1:nrow(con$field),1:ncol(con$field)+2] <-m[1:nrow(con$field),1:ncol(con$field)+2]+con$field
   
-  m[1:nrow(M)+1,1:ncol(M)] <-m[1:nrow(M)+1,1:ncol(M)]+M
+  m[1:nrow(con$field)+1,1:ncol(con$field)] <-m[1:nrow(con$field)+1,1:ncol(con$field)]+con$field
   
-  m[1:nrow(M)+1,1:ncol(M)+2] <-m[1:nrow(M)+1,1:ncol(M)+2]+M
+  m[1:nrow(con$field)+1,1:ncol(con$field)+2] <-m[1:nrow(con$field)+1,1:ncol(con$field)+2]+con$field
   
-  m[1:nrow(M)+2,1:ncol(M)] <-m[1:nrow(M)+2,1:ncol(M)]+M
-  m[1:nrow(M)+2,1:ncol(M)+1] <-m[1:nrow(M)+2,1:ncol(M)+1]+M
-  m[1:nrow(M)+2,1:ncol(M)+2] <-m[1:nrow(M)+2,1:ncol(M)+2]+M
-  
+  m[1:nrow(con$field)+2,1:ncol(con$field)] <-m[1:nrow(con$field)+2,1:ncol(con$field)]+con$field
+  m[1:nrow(con$field)+2,1:ncol(con$field)+1] <-m[1:nrow(con$field)+2,1:ncol(con$field)+1]+con$field
+  m[1:nrow(con$field)+2,1:ncol(con$field)+2] <-m[1:nrow(con$field)+2,1:ncol(con$field)+2]+con$field
   return(m)
 }
 
@@ -100,7 +126,7 @@ Verkleinern <- function(M){
 #' @param 
 #' @return 
 #' @examples
-Update <- function(M,R = "23/3"){
+update.conway <- function(con){
 
   # Update bekommt eine Matrix und einen Regelsatz
   # Update liefert eine Upgedatete Matrix
@@ -109,23 +135,22 @@ Update <- function(M,R = "23/3"){
   #
   # R = Regeln "Bestehen/Entstehen"
   
-  if(is.character(R)){
-    REGELN = alsRegelliste(R)
+  if(is.character(con$rules)){
+    REGELN = alsRegelliste(con$rules)
   }else{
-    REGELN = R
+    REGELN = con$rules
   }
   
-  AnzahlNachbarn = Nachbarn(M)
+  AnzahlNachbarn = Nachbarn(con)
   
-  m <- array(0,dim(M)+2)              # Alte Matrix mit der Dimension der Nachtbarmatrix
-  m[-c(1,nrow(m)),-c(1,ncol(m))]<- M  
+  m <- array(0,dim(con$field)+2)              # Alte Matrix mit der Dimension der Nachtbarmatrix
+  m[-c(1,nrow(m)),-c(1,ncol(m))]<- con$field  
   
-  
-  Ergebnis <- array(0,dim(M)+2)
+  Ergebnis <- array(0,dim(con$field)+2)
   Ergebnis <- apply(((AnzahlNachbarn %in% REGELN$BESTEHEN )& m | AnzahlNachbarn %in% REGELN$ENTSTEHEN),c(1,2),as.integer)
   Ergebnis <- Verkleinern(Ergebnis)
-  return(Ergebnis)
-  
+  con$field <- Ergebnis
+  return(con)
 }
 
 
@@ -134,16 +159,28 @@ Update <- function(M,R = "23/3"){
 #' @param 
 #' @return 
 #' @examples
-plotConway <- function(M){
+plot.conway <- function(con){
   par(bg = 'black')
-  image(M,col = gray.colors(2,0,1),
+  image(con$field,col = gray.colors(2,0,1),
         asp=1,
-        x = seq(0,nrow(M)-1,length.out = nrow(M)),
-        y=seq(0,ncol(M)-1,length.out = ncol(M)),
+        x = seq(0,nrow(con$field)-1,length.out = nrow(con$field)),
+        y=seq(0,ncol(con$field)-1,length.out = ncol(con$field)),
         xaxt='n',yaxt = 'n' , ann = FALSE,bty="n")
-  mtext(side = 1, text = nrow(M), line = 1, col = "white")
-  mtext(side = 2, text = ncol(M), line = 1, col = "white")
+  mtext(side = 1, text = nrow(con$field), line = 1, col = "white")
+  mtext(side = 2, text = ncol(con$field), line = 1, col = "white")
   
+}
+
+
+summary.conway <- function(x, ...) {
+  rows <- nrow(x$field)
+  cols <- ncol(x$field)
+  n_life <- sum(x$field)
+  n_dead <- rows * cols - n_life
+  print(paste("rows = ", rows))
+  print(paste("columns = ", cols))
+  print(paste("living cells = ", n_life))
+  print(paste("dead cells = ", n_dead))
 }
 
 ########################
@@ -174,12 +211,15 @@ explusion = function(){
 rPentomino = function(){
   return(matrix(c(0,1,0,1,1,1,1,0,0),3,3))
 }
+
+
 ########################
 #                      #
 #   Kleines Beispiel   #
 #                      #
 ########################
-x = Zufallsmatrix(50,100,0.1)
+#x <- Zufallsmatrix(50,100,0.1)
+x <- conway("random", p = 0.2, x=100)
 #y = (x + Update(x))/2
 #x = Update(x)
 
@@ -187,9 +227,9 @@ x = Zufallsmatrix(50,100,0.1)
 #x = Update(x)
 #x = array(c(1,0),c(51,51))
 for(i in 1:1000){
-  plotConway(x)
-  x = Update(x)#,R = "12345/3	")
+  plot(x)
+  x <- update(x)#,R = "12345/3	")
   Sys.sleep(0.2)
 }
 
-plotConway(array(runif(100,min = 0,max = 2),c(1000,10)))
+#plotConway(array(runif(100,min = 0,max = 2),c(1000,10)))
